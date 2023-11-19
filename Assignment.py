d4a1718fde8f2ce0ecaf7dcb3bc168f65e7861d5
Immaculate5 <146907231+Immaculate5@users.noreply.github.com>
2023-11-19 12:15:01 -0800

    first commit

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+#start_value=0
+#stop_value=10
+#for i in range(start_value, stop_value):
+   # print(i)
+
+
+for i in range(1, 11):
+    print(i)
+
---
+++ for loop.py
@@ -0,0 +1,7 @@
+#For loops using range
+#A range is aseries of values between two numeric intervals
+#we use python's built-in function range() to define a range of values.
+welcome_messages="Welcome to PLP"
+for welcome_message in welcome_messages:
+    #1, 2, 3, 4, 5
+    print(welcome_messages)
